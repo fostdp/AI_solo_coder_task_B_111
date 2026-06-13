cde9f4cb82640b8c59a840625c99e7a45cd1f677
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-13 17:56:56 +0800

    Update project

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,7 +5,11 @@ const routes = [
   { path: '/bridge', component: () => import('../views/BridgeMonitor.vue') },
   { path: '/simulation', component: () => import('../views/Simulation.vue') },
   { path: '/damage', component: () => import('../views/DamagePrediction.vue') },
-  { path: '/alerts', component: () => import('../views/Alerts.vue') }
+  { path: '/alerts', component: () => import('../views/Alerts.vue') },
+  { path: '/weathering', component: () => import('../views/WeatheringAssessment.vue') },
+  { path: '/traffic', component: () => import('../views/TrafficImpact.vue') },
+  { path: '/masonry', component: () => import('../views/MasonryTech.vue') },
+  { path: '/priority', component: () => import('../views/PriorityRanking.vue') }
 ]
 
 export default createRouter({
